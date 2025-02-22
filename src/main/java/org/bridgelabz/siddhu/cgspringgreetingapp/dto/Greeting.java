f8ff1172d63a37f05257a03bf538ec63343f93f1
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-23 03:33:36 +0530

    ✨[Sudhakar] Add. added Greeting with first name and last name feature

--- src/main/java/org/bridgelabz/siddhu/cgspringgreetingapp/dto/Greeting.java
+++ src/main/java/org/bridgelabz/siddhu/cgspringgreetingapp/dto/Greeting.java
@@ -2,9 +2,22 @@ package org.bridgelabz.siddhu.cgspringgreetingapp.dto;
 
 public class Greeting {
     private final String message;
+    private final String firstName;
+    private final String lastName;
 
-    public Greeting(String message) {
+    public Greeting(String message, String firstName, String lastName) {
         this.message = message;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public String getMessage() {
