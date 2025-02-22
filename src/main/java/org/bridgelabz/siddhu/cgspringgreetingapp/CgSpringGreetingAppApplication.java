0c20551d255ba88aefa5a876e5b2f9afd8f3e366
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-23 00:01:29 +0530

    🎉 Initial Commit

---
+++ src/main/java/org/bridgelabz/siddhu/cgspringgreetingapp/CgSpringGreetingAppApplication.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.cgspringgreetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgSpringGreetingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgSpringGreetingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bridgelabz/siddhu/cgspringgreetingapp/CgSpringGreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.cgspringgreetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgSpringGreetingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
