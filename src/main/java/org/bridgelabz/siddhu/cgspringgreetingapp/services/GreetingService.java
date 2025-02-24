327ffd2b3a4fcdad1c612f29538c8a288ba2e638
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-24 11:18:17 +0530

    ✨[Sudhakar] Add. Implemented get All greeting function to edit a greeting message using Id

--- src/main/java/org/bridgelabz/siddhu/cgspringgreetingapp/services/GreetingService.java
+++ src/main/java/org/bridgelabz/siddhu/cgspringgreetingapp/services/GreetingService.java
@@ -1,12 +1,25 @@
 package org.bridgelabz.siddhu.cgspringgreetingapp.services;
 
 import org.bridgelabz.siddhu.cgspringgreetingapp.dto.Greeting;
+import org.bridgelabz.siddhu.cgspringgreetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GreetingService {
+    private final GreetingRepository greetingRepository;
+    @Autowired
+    GreetingRepository greetingRepository1;
+
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Deprecated
     public Greeting createGreeting(String firstName,String lastName){
+
 
         String message;
         if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()){
@@ -25,5 +38,16 @@ public class GreetingService {
 
     public Greeting getSimpleGreeting(){
         return new Greeting("Hello World",null,null);
+    }
+    public Greeting updateGreeting(long id, String greeting){
+        Optional<Greeting> optionalGreeting = greetingRepository1.findById(id);
+        if(optionalGreeting.isPresent()){
+            Greeting greeting1 = optionalGreeting.get();
+            greeting1.setMessage(greeting);
+            return greetingRepository.save(greeting1);
+        }
+        else{
+            return null;
+        }
     }
 }
